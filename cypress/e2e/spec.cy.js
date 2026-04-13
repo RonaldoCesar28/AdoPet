61bc54ca0229469922d1504a033819baac39cc35
RonaldoCesar28 <ronaldocesar28@outlook.com>
2026-04-13 19:41:10 -0300

    fazendo cadastro na adopet

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Ronaldo Cesar');
+    cy.get('input[name="email"]').type('ronaldocesar@teste.com.br');
+    cy.get('input[name="password"]').type('Senha12345');
+    cy.get('input[name="confirm_password"]').type('Senha12345');
+    cy.contains('button', 'Cadastrar').click();
   })
 })
